9a58a5c5bbac2043f1ac9d78ab486907ccf48c5d
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2021-04-13 00:47:10 +0530

    checking jinja template

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template
 
 app = Flask(__name__)
 
+dummy_data = [
+    {
+        'author': 'John Lee',
+        'title': 'A Beautiful World',
+        'content': 'This is a sample data to fill in the void.',
+        'date': '01 April 2021'
+    },
+    {
+        'author': 'Mary Williams',
+        'title': 'Woman Power',
+        'content': 'Women are the secret power to the society.',
+        'date': '05 April 2020'
+    }
+]
+
+# home page
 @app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'<h1>Hello, {escape(name)}!</h1>'
+@app.route('/home')
+def home():
+    return render_template('./home/home.html', data=dummy_data)
+
+# about page
+@app.route('/about')
+def about():
+    return render_template('./about/about.html', title='AboutUs')
 
 if __name__ == '__main__':
     app.run(debug=True)
