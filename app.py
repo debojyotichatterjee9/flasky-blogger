96fe25fc6d4b9cca4509a75c7b71a9aa2de61a37
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2021-04-18 18:11:49 +0530

    building forms for the app

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, escape, request, render_template, url_for
+from custom_modules.forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+# secret key for the application
+app.config['SECRET_KEY'] = 'afe5ae8b579a863afb52b97354a1c9c649e3055fbf5165d127b29650ad904ee5'
 
 dummy_data = [
     {
@@ -18,15 +22,30 @@ dummy_data = [
 ]
 
 # home page
+
+
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('./home/home.html', data=dummy_data)
 
 # about page
+
+
 @app.route('/about')
 def about():
     return render_template('./about/about.html', title='AboutUs')
+
+
+@app.route('/register')
+def register():
+    reg_form = RegistrationForm()
+    return render_template('./register/register.html', title='Registration', form=reg_form)
+
+@app.route('/login')
+def login():
+    login_form = LoginForm()
+    return render_template('./login/login.html', title='Login', form=login_form)
 
 # if running this file directly using python then the below condition is true
 if __name__ == '__main__':
---
+++ custom_modules/forms.py
@@ -0,0 +1,40 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[
+                               DataRequired(),
+                               Length(min=2, max=20)
+                           ])
+    email = StringField('Email',
+                        validators=[
+                            DataRequired(),
+                            Email()
+                        ])
+    password = StringField('Password',
+                           validators=[
+                               DataRequired(),
+                           ])
+    confirm_password = StringField('Re-type Password',
+                                   validators=[
+                                       DataRequired(),
+                                       EqualTo('password')
+                                   ])
+    submit = SubmitField('Register')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[
+                            DataRequired(),
+                            Email()
+                        ])
+    password = StringField('Password',
+                           validators=[
+                               DataRequired(),
+                           ])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
