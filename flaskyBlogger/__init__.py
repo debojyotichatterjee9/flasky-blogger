2c16fa4d027e2f3c3aaa4ae0c9aa3e3ecb0a4168
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2021-04-28 21:39:56 +0530

    setting the configs

--- flaskyBlogger/__init__.py
+++ flaskyBlogger/__init__.py
@@ -4,24 +4,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flaskyBlogger.config import Config
 
 app = Flask(__name__)
-
-# secret key for the application
-app.config['SECRET_KEY'] = 'afe5ae8b579a863afb52b97354a1c9c649e3055fbf5165d127b29650ad904ee5'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'warning'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-# set environment variables for sensitive data
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')
 mail = Mail(app)
 
 
