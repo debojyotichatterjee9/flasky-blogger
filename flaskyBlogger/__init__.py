f8d2ca777bbab71abe5227d549a7929a82b0d4a2
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2021-04-28 22:26:11 +0530

    error pages and routes updated

--- flaskyBlogger/__init__.py
+++ flaskyBlogger/__init__.py
@@ -16,22 +16,28 @@ mail = Mail()
 
 
 # from flaskyBlogger.routes import routes
-from flaskyBlogger.main.routes import main
-from flaskyBlogger.users.routes import users
-from flaskyBlogger.posts.routes import posts
+
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
+    
     print(app.config['MAIL_USERNAME'])
     print(app.config['MAIL_PASSWORD'])
+    
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    
+    from flaskyBlogger.main.routes import main
+    from flaskyBlogger.users.routes import users
+    from flaskyBlogger.posts.routes import posts
+    from flaskyBlogger.errors.error_handlers import errors
     # registering the blueprints
     app.register_blueprint(main)
     app.register_blueprint(users)
     app.register_blueprint(posts)
+    app.register_blueprint(errors)
 
     return app
