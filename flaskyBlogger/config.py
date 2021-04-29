fcc74b24afb42073e3712b4ffc20e7ebf6d38c82
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2021-04-29 13:08:20 +0530

    update in db uri

--- flaskyBlogger/config.py
+++ flaskyBlogger/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = "postgres://vemvuchbsdejiu:2ed5608887a83dda65732569f757d343aaefa4788d9e4e9da3154e293a772bd7@ec2-54-196-33-23.compute-1.amazonaws.com:5432/d842aln8tlhs97"
+    SQLALCHEMY_DATABASE_URI = "postgresql://vemvuchbsdejiu:2ed5608887a83dda65732569f757d343aaefa4788d9e4e9da3154e293a772bd7@ec2-54-196-33-23.compute-1.amazonaws.com:5432/d842aln8tlhs97"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
