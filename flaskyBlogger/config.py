02afe192e32252e0a16aa6598f231de94b21f47c
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2021-04-29 16:58:45 +0530

    minor changes

--- flaskyBlogger/config.py
+++ flaskyBlogger/config.py
@@ -1,6 +1,5 @@
 import os
 
-
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = "postgresql://vemvuchbsdejiu:2ed5608887a83dda65732569f757d343aaefa4788d9e4e9da3154e293a772bd7@ec2-54-196-33-23.compute-1.amazonaws.com:5432/d842aln8tlhs97"
@@ -8,5 +7,5 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
